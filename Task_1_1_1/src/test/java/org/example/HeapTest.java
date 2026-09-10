245aec0d931140d1190a85636d32538cc7e571a1
pavelfedorenko <pavel.fedorenko0602@gmail.com>
2026-09-10 21:15:34 +0700

    добавил тесты

--- Task_1_1_1/src/test/java/org/example/HeapTest.java
+++ Task_1_1_1/src/test/java/org/example/HeapTest.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,7 @@ class HeapTest {
 
     @Test
     void extractMax() {
-        int[] array = {5, 4, 3, 2, 1};
+        int[] array = {4, 3, 5, 2, 1};
         var heap = new Heap(array);
 
         assertEquals(5, heap.extractMax());
@@ -17,9 +18,22 @@ class HeapTest {
 
     @Test
     void extractMaxNoElements() {
-        int[] array = new int[]{};
-        var heap = new Heap(array);
+        Heap heap = new Heap(new int[]{});
 
-        assertEquals(-1, heap.extractMax());
+        assertThrows(
+                IllegalStateException.class,
+                heap::extractMax
+        );
+    }
+
+    @Test
+    void extractSeveralElements() {
+        int[] array = {3, 5, 2, 1};
+        Heap heap = new Heap(array);
+
+        assertEquals(5, heap.extractMax());
+        assertEquals(3, heap.extractMax());
+        assertEquals(2, heap.extractMax());
+        assertEquals(1, heap.extractMax());
     }
 }
