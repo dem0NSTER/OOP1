3f23094ae67c83e0096d328f53ce2e9e7df20bde
pavelfedorenko <pavel.fedorenko0602@gmail.com>
2026-09-15 12:58:58 +0700

    модель карты

---
+++ Task_1_1_2/src/main/java/org/example/cards/Rank.java
@@ -0,0 +1,17 @@
+package org.example.cards;
+
+public enum Rank {
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING,
+    ACE
+}
---
+++ Task_1_1_2/src/main/java/org/example/cards/Suit.java
@@ -0,0 +1,8 @@
+package org.example.cards;
+
+public enum Suit {
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES
+}
